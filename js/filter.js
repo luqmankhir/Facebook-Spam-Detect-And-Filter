cf35ad0b7fb85fb63a185256273c91c3f2d4de88
Luq Tozaki <manluq_46@yahoo.com>
2023-05-31 01:30:21 +0800

    Prompt message to the HTML page and scanning using IntersectionObserver
    This time the message will prompt at the page itself and when the user click 'OK' it will filter the whole spam keywords.

--- js/filter.js
+++ js/filter.js
@@ -6,7 +6,8 @@
 
 var xpathPatterns = [];
 
-
+//Empty array to stop scanning when user click cancel to spam keywords that they don't want to scan
+var filteredKeywords = [];
 
 
 chrome.storage.sync.get({
@@ -69,6 +70,14 @@ function confirmFilterKeyword(keyword) {
 
 
 //Prompt error before block the spam keywords
+/*
+    1) There's an issue where user will always need to prompt 'OK' on every each of keywords that had been detected
+    2) Everytime user click cancel and continue scrolling the web page, the prompt message will popup back
+        whether i would like to continue block the keyword that i just click Cancel.
+    3) Hovering issue everytime I hover to something, the web page will regenerate and return scanning the whole page
+        back    
+*/
+/*
 function filterNodes() {
     var filteredNodes = [];
   
@@ -102,10 +111,96 @@ function filterNodes() {
 window.addEventListener("load", function() {
     filterNodes()
 });
+*/
 
-setTimeout(filterNodes, 1000);
+//Prompt error before block the spam keywords
+var isFiltering = false;
+
+function filterNodes() {
+    // Set the flag to indicate that filtering is in progress
+    isFiltering = true;
+  
+    var array = new Array();
+    for (i = 0; i < xpathPatterns.length; i++) {
+      var xpathResult = document.evaluate(
+        xpathPatterns[i][0],
+        document,
+        null,
+        XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
+        null
+      );
+      var thisNode = xpathResult.iterateNext();
+      while (thisNode) {
+        // only when we have a match for the whole word add the node to the array
+        // actually, we allow for variations like "_word_" "words" or "_words"
+        var regex = new RegExp(
+          "(\\b|_)(" + xpathPatterns[i][1] + ")(\\b|_|s)",
+          "i"
+        );
+        if (regex.test(thisNode.data)) {
+          array.push(thisNode);
+        }
+        thisNode = xpathResult.iterateNext();
+      }
+    }
+  
+    for (var i = 0; i < array.length; i++) {
+      var p = array[i].parentNode;
+      if (p !== null) p.removeChild(array[i]);
+    }
+  
+    // Set the flag to indicate that filtering is complete
+    isFiltering = false;
+  }
+ 
+  
+
+
+
+/*setTimeout(filterNodes, 1000);
 setInterval(filterNodes, 2000);
 
 window.addEventListener("scroll", function() {
     filterNodes()
+});
+*/
+
+
+
+// Initialize a flag to track if filtering is already in progress
+var isFiltering = false;
+
+window.addEventListener("load", function() {
+  if (!isFiltering) {
+    filterNodes();
+  }
+});
+
+setTimeout(function() {
+  if (!isFiltering) {
+    filterNodes();
+  }
+}, 1000);
+
+setInterval(function() {
+  if (!isFiltering) {
+    filterNodes();
+  }
+}, 2000);
+
+window.addEventListener("scroll", function() {
+  // Check if the filtering is already in progress, if yes, return immediately
+  if (isFiltering) {
+    return;
+  }
+
+  // Set a timeout to delay the filtering process after scrolling stops
+  if (scrollTimeout) {
+    clearTimeout(scrollTimeout);
+  }
+
+  // Start a new timeout to trigger the filtering after 500 milliseconds of scrolling inactivity
+  scrollTimeout = setTimeout(function() {
+    filterNodes();
+  }, 500);
 });
