236eb588f23547ede3c4d84f20d3e663efb974cb
Luq Tozaki <manluq_46@yahoo.com>
2023-06-29 22:27:28 +0800

    Update spamkeywords.js

--- js/spamkeywords.js
+++ js/spamkeywords.js
@@ -1,3 +1,3 @@
-var sourceCodeKeywords = ['Trump','trump','TRUMP','AMERICA','America','america','a55','a55hole','aeolus','ahole',
+var sourceCodeKeywords = ['a55','a55hole','aeolus','ahole',
 'anal'
 ];
